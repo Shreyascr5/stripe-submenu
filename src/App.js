34d732c3bdfa6e57f8c48856fbef7d8f53aec131
Shreyascr5 <shreyas050801@gmail.com>
2023-08-13 13:53:48 +0530

    created custom hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React from 'react';
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero"
+import Submenu from "./Submenu";
 
 function App() {
   return (
-   <></>
+   <>
+    <Navbar />
+    <Sidebar/>
+    <Hero/>
+    <Submenu/>
+   </>
   );
 }
 
--- src/context.js
+++ src/context.js
@@ -1,2 +1,37 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+import { createContext } from 'react';
+
+
+const AppContext=createContext();
+
+const AppProvider=(children)=>{
+    const[isSidebarOpen,setIsSidebarOpen]=useState(true);
+    const[isSubmenuOpen,setIsSubmenuOpen]=useState(true);
+
+
+  const openSidebar=()=>{
+    setIsSidebarOpen(true);
+  }  
+  const closesidebar=()=>{
+    setIsSidebarOpen(false);
+  }
+  const openSubmenu=()=>{
+    setIsSubmenuOpen(true);
+  }  
+  const closeSubmenu=()=>{
+    setIsSubmenuOpen(false);
+  }
+    return(<AppContext.Provider value={{
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closesidebar,
+        isSubmenuOpen,
+        isSidebarOpen
+    }}>{children}</AppContext.Provider>)
+}
+export const useGLobalContext=()=>{return(useContext(AppContext))}
+
+export {AppContext,AppProvider};
+
